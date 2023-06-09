528b512b062c8d630dba46261fee46c5c01db499
eusnol-an <dev.eunsol@gmail.com>
2023-06-09 18:29:48 +0900

    initial commit

---
+++ src/test/java/com/example/argumentresolver/ArgumentResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.argumentresolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgumentResolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
